e8a3cc1d8b36a297a5a25bc427a0c2029848296f
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2019-06-16 15:34:13 +0200

    Add global setup script

---
+++ setup/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+${DIR}/00-basic.sh
+${DIR}/01-java.sh
+${DIR}/02-node.sh
+${DIR}/03-maven.sh
+${DIR}/04-docker.sh
+${DIR}/05-browsers.sh
