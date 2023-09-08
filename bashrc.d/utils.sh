a24be6cc8f0a00094df5d7003e49731820be53c3
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2023-09-08 20:40:28 +0200

    Better output

--- bashrc.d/utils.sh
+++ bashrc.d/utils.sh
@@ -10,10 +10,10 @@ function git-up {
   fi
 
   for dir in *; do
-    pushd $dir
-    if [[ -d "./.git" ]]; then
-      git pull --rebase
+    if [[ -d "$dir/.git" ]]; then
+      echo "\033[0;32mUpdating $dir...\033[0m"
+      (cd $dir && git pull --rebase)
+      echo ""
     fi
-    popd;
   done
 }
